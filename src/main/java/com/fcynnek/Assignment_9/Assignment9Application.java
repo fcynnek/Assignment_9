79312cd86a1e612c06e0417a32726397d223fd38
fcynnek <kchengfung@gmail.com>
2023-03-07 16:42:07 -0500

    Testing

--- src/main/java/com/fcynnek/Assignment_9/Assignment9Application.java
+++ src/main/java/com/fcynnek/Assignment_9/Assignment9Application.java
@@ -1,13 +1,22 @@
 package com.fcynnek.Assignment_9;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.fcynnek.Assignment_9.service.FileService;
 
 @SpringBootApplication
 public class Assignment9Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Assignment9Application.class, args);
+	public static void main(String[] args) throws IOException {
+//		SpringApplication.run(Assignment9Application.class, args);
+		
+		FileService fs = new FileService();
+		
+		System.out.println(fs.readFile("recipes.txt").toString());
+		
 	}
 
 }
