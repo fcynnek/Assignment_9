980c28f2f1ed3b2d7ebf34779fc7b4b484e717f5
fcynnek <kchengfung@gmail.com>
2023-03-06 19:05:34 -0500

    Consuming the data

--- src/main/java/com/fcynnek/Assignment_9/service/FileService.java
+++ src/main/java/com/fcynnek/Assignment_9/service/FileService.java
@@ -13,21 +13,21 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
-import com.fcynnek.Assignment_9.repository.RecipeRepository;
+import com.fcynnek.Assignment_9.repository.Recipe;
 
 @Service
 public class FileService {
 //Ingest a recipe.txt file and store this information in a Java Collection
 	
 	private String filename;
+	List<Recipe> recipes = new ArrayList<>();
 	
 	public FileService() {
 		
 	}
 	
-	public List<RecipeRepository> readFile (String filename) throws IOException {
+	public List<Recipe> readFile (String filename) throws IOException {
 		Reader in = new FileReader(filename);
-		List<RecipeRepository> recipes = new ArrayList<>();
 		Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
 		for (CSVRecord record : records) {
 			Boolean dairy = Boolean.parseBoolean(record.get("Dairy Free"));
@@ -35,7 +35,7 @@ public class FileService {
 			Boolean vegan	= Boolean.parseBoolean(record.get("Vegan"));
 			Boolean vegetarian	= Boolean.parseBoolean(record.get("Vegetarian"));
 			if ((dairy == true) || (gluten == true) || (vegan == true) || (vegetarian == true)) {				
-//				RecipeRepository recipe = new RecipeRepository();
+//				Recipe recipe = new Recipe();
 				String title	= record.get("Title");
 				Double score	= Double.parseDouble(record.get("Spoonacular Score"));
 				Integer servings	= Integer.parseInt(record.get("Servings"));
@@ -45,15 +45,15 @@ public class FileService {
 				Integer readyMins	= Integer.parseInt(record.get("Ready In Minutes"));
 				String instructions	= record.get("Instructions");
 				
-				RecipeRepository recipe = new RecipeRepository(readyMins, instructions, priceServ, priceServ, readyMins, readyMins, priceServ, instructions);
-				recipe.setTitle(title);
-				recipe.setSpoonacularScore(score);
-				recipe.setServings(servings);
-				recipe.setPricePerServing(priceServ);
-				recipe.setPreparationMinutes(prepMins);
-				recipe.setCookingMinutes(cookingMins);
-				recipe.setReadyInMinutes(readyMins);
-				recipe.setInstructions(instructions);
+				Recipe recipe = new Recipe(title, score, servings, priceServ, prepMins, cookingMins, readyMins, instructions);
+//				recipe.setTitle(title);
+//				recipe.setSpoonacularScore(score);
+//				recipe.setServings(servings);
+//				recipe.setPricePerServing(priceServ);
+//				recipe.setPreparationMinutes(prepMins);
+//				recipe.setCookingMinutes(cookingMins);
+//				recipe.setReadyInMinutes(readyMins);
+//				recipe.setInstructions(instructions);
 				recipes.add(recipe);
 			}
 		}
@@ -61,7 +61,9 @@ public class FileService {
 		return recipes;
 	}
 	
-	public RecipeRepository[] dairyFree() {
+	public Recipe[] dairyFree() {
+		
+//		recipes.
 		
 		
 		return null;
