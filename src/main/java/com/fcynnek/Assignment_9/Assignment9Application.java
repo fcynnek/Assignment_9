c812e62a14344c21edbd94b5555eeb345abf6d5e
fcynnek <kchengfung@gmail.com>
2023-03-07 19:15:53 -0500

    Testing

--- src/main/java/com/fcynnek/Assignment_9/Assignment9Application.java
+++ src/main/java/com/fcynnek/Assignment_9/Assignment9Application.java
@@ -1,10 +1,13 @@
 package com.fcynnek.Assignment_9;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fcynnek.Assignment_9.repository.Recipe;
 import com.fcynnek.Assignment_9.service.FileService;
 
 @SpringBootApplication
@@ -15,7 +18,11 @@ public class Assignment9Application {
 		
 		FileService fs = new FileService();
 		
-		System.out.println(fs.readFile("recipes.txt").toString());
+		System.out.println(fs.readFile("recipes.txt"));
+		
+		System.out.println(fs.dairyFree());
+		
+		
 		
 	}
 
